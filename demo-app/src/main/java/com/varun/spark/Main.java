bb5de6552cb0b76ce218f3616046522625d4e3c7
varungoyal1994 <varungoyal1994@gmail.com>
2025-02-13 00:55:51 +0530

    Code cleanup

--- demo-app/src/main/java/com/varun/spark/Main.java
+++ demo-app/src/main/java/com/varun/spark/Main.java
@@ -1,13 +1,20 @@
 package com.varun.spark;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+
 import org.apache.spark.SparkConf;
 import org.apache.spark.sql.Dataset;
 import org.apache.spark.sql.Row;
 import org.apache.spark.sql.SparkSession;
-
+import static org.apache.spark.sql.functions.*;
 public class Main {
     public static void main(String[] args) {
-        System.setProperty("hadoop.home.dir", "D:\\Coding\\Projects\\Java\\Spark\\SPARK-DEMO\\demo-app\\src\\main\\resources\\winutils\\hadoop-3.3.1");
+        String path = "demo-app/src/main/resources/";
+        Path relativePath = Paths.get(path);
+        System.out.println("Path is " + relativePath.toAbsolutePath() );
+
+       // System.setProperty("hadoop.home.dir", path);
         System.setProperty("java.opts", "-Xms2g -Xmx4g"); // Increase JVM heap size
         //For above java.opts JRE ignores Xms2g and other values while jdk doesnt. IN that case in prod memeory options turning is done carefully.
         //we usually submit spark job to yarn so we dont care much
@@ -20,10 +27,11 @@ public class Main {
 
         System.out.println("Hello world!" + conf.get("spark.driver.memory"));
         SparkSession sc = SparkSession.builder().config(conf).getOrCreate();
-        Dataset<Row> dataframe = sc.read().format("csv").option("header", true).load("D:\\Coding\\Projects\\Java\\Spark\\SPARK-DEMO\\demo-app\\src\\main\\resources\\name_and_comments.txt");
+        Dataset<Row> dataframe = sc.read().format("csv").option("header", true)
+            .load(path + "name_and_comments.txt");
         dataframe.show();
         System.out.println("Data frames are immuatable after this still no new col");
-        dataframe.withColumn("New Name", dataframe.col("first_name"));
+        dataframe.withColumn("New Name", concat( dataframe.col("first_name"), lit(" "), dataframe.col("last_name")));
         dataframe.show();
         
     }
